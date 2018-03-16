82583d3256d89dcdba2711d755981a46da7376b3
jisung <jisung.smart@gmail.com>
2018-03-17 07:25:36 +0900

    Initial commit

---
+++ NanoDegree/Meme1.0/Meme1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by The book on 2018. 1. 1..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
